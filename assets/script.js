acd73e87ed38cf74ff23f1acad511c7e9ae6e7f5
BerserkFalcon <lordscott90@gmail.com>
2025-01-23 09:06:27 -0500

    base site

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,15 @@
 const recipes = [];
 const searchButton = document.getElementById("searchButton");
-console.log(searchButton);
-const searchButton2 = document.querySelector('.addButton')
-console.log(searchButton2);
+const addButton = document.querySelector('.addButton');
+const page1 = document.getElementById("page1");
+
+//returns random val 1 time(s)
+function randVal(){
+    for (let i=0;i<1;i++){
+        var val = Math.floor(Math.random()*9); //rand val = 9
+        return val;
+    }
+}
+//assigns function to clicking add
+addButton.addEventListener('click', function(){randVal()});
+page1.textContent = randVal();
